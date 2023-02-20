fefb6beabf56d8645ecf2b4c74cb9dde816cf806
LuisGarcia <luis_garcia_11@outlook.es>
2023-02-19 18:13:58 -0600

    Follower endpoints

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -3,6 +3,7 @@ import {Router} from 'express';
 import {authRouter} from '../routes/auth.router';
 import {creatorRouter} from '../routes/creator.router';
 import {videoRouter} from '../routes/video.router';
+import { followerRouter } from './follower.router';
 import { likevideoRouter } from './likevideo.router';
 
 const api = Router();
@@ -11,7 +12,7 @@ api.use('/auth', authRouter);
 api.use('/creator', creatorRouter);
 api.use('/video', videoRouter);
 api.use('/like', likevideoRouter);
-api.use('/follower', likevideoRouter);
+api.use('/follower', followerRouter);
 
 export {
     api
